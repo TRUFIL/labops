fc3bedb51d39b581f44b89dce70c59a019d9a49c
Shweta <shweta@dgsol-in.com>
2017-01-09 07:12:57 -0800

    locations.py is updated with requied validation

--- labops/transformers/doctype/functional_locations/functional_locations.py
+++ labops/transformers/doctype/functional_locations/functional_locations.py
@@ -4,7 +4,30 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class FunctionalLocations(Document):
-	pass
+	def validate(self):
+		self.check_mandatory_fields()
+
+	def check_mandatory_fields(self):
+		if (not self.fl_cd):
+			frappe.throw(_("Location Code is Mandatory... Provide Customer given value or Type unique value to identify it"), frappe.MandatoryError)
+
+		if ((not self.fl_fl) or (not self.fl_ss)):
+			frappe.throw(_("Substation and Fuctional Location is Mandatory... Provide value"), frappe.MandatoryError)
+		elif (not self.fl_plant):
+			self.fl_loc_desc = generate_fl_loc_desc(self.fl_fl, self.fl_ss)
+		else:
+			self.fl_loc_desc = generate_fl_loc_desc(self.fl_fl, self.fl_ss, self.fl_plant)
+
+
+# Supporting Functions for Class FunctionalLocations
+
+def generate_fl_loc_desc(fl, ss, plant=str('')):
+	if (not plant):
+		return ss.strip().upper() + "_" + fl.strip().upper()
+	else:
+		return plant.strip().upper() + "_" + ss.strip().upper() + "_" + fl.strip().upper()
+
